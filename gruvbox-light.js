5b2d26bc86eafa0fa77048837773c9ff82660a00
Sebastian Bean <seb@bean.la>
2026-05-20 16:31:06 -0400

    Initial: Blink Shell Gruvbox Light theme

---
+++ gruvbox-light.js
@@ -0,0 +1,26 @@
+black       = '#fbf1c7';
+red         = '#cc241d';
+green       = '#98971a';
+yellow      = '#d79921';
+blue        = '#458588';
+magenta     = '#b16286';
+cyan        = '#689d6a';
+white       = '#7c6f64';
+lightBlack  = '#928374';
+lightRed    = '#9d0006';
+lightGreen  = '#79740e';
+lightYellow = '#b57614';
+lightBlue   = '#076678';
+lightMagenta= '#8f3f71';
+lightCyan   = '#427b58';
+lightWhite  = '#3c3836';
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', 'rgba(0, 0, 0, 0.5)');
+t.prefs_.set('foreground-color', '#3c3836');
+t.prefs_.set('background-color', '#fbf1c7');
