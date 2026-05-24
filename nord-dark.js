41768814250828ce9bbd137112d3835d10fdb487
Sebastian Bean <seb@bean.la>
2026-05-23 21:49:13 -0400

    add nord-dark theme for Blink Shell

---
+++ nord-dark.js
@@ -0,0 +1,26 @@
+black       = '#2e3440';
+red         = '#bf616a';
+green       = '#a3be8c';
+yellow      = '#ebcb8b';
+blue        = '#81a1c1';
+magenta     = '#b48ead';
+cyan        = '#88c0d0';
+white       = '#d8dee9';
+lightBlack  = '#4c566a';
+lightRed    = '#bf616a';
+lightGreen  = '#a3be8c';
+lightYellow = '#ebcb8b';
+lightBlue   = '#5e81ac';
+lightMagenta= '#b48ead';
+lightCyan   = '#8fbcbb';
+lightWhite  = '#eceff4';
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', 'rgba(136, 192, 208, 0.5)');
+t.prefs_.set('foreground-color', '#d8dee9');
+t.prefs_.set('background-color', '#2e3440');
